5f9c277466d8b50335fc5e374f495bf43c985aac
khoang-dev <73509397+khoang-dev@users.noreply.github.com>
2024-04-08 13:24:06 +0700

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -15,7 +15,7 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/page", function (req, res) {
   res.sendFile(__dirname + "../../views/index.html");
 });
 
