f8737802ff3653b13a8cd37a359d0ac739640173
khoang-dev <73509397+khoang-dev@users.noreply.github.com>
2024-04-08 11:10:45 +0700

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,6 +6,6 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+app.use('/.netlify/functions/api', router);
 
 export const handler = serverless(api);
